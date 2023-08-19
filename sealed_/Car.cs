a1b74932f2c21b5a6a042df690bdab5c744ddc3c
zuxriddinCsharp <137428899+zuxriddinCsharp@users.noreply.github.com>
2023-08-19 18:26:54 +0500

    Добавьте файлы проекта.

---
+++ sealed_/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sealed_
+{
+    internal class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; }
+        public static decimal TaxPercentage { get; set; }
+
+        public Car(int id, string model, string brand, decimal price, decimal taxPercentage)
+        {
+            Id = id;
+            Model = model;
+            Brand = brand;
+            Price = price;
+            TaxPercentage = taxPercentage;
+        }
+
+        public decimal CalaulateTotal()
+        {
+            return Price - (Price * TaxPercentage / 100);
+        }
+    }
+}
---
+++ sealed_/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace sealed_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(1, "Cobat", "Chevrolet", 100_000, 0);
+            Car car1 = new Car(2, "Lacetti", "Chevrolt", 161_000, 0);
+            Car car2 = new Car(3, "Xray", "Lada", 170_000, 0);
+            Car car3 = new Car(4, "Prado", "Toyota", 300_000, 0);
+
+            Console.WriteLine(car.CalaulateTotal());
+            Console.WriteLine(car1.CalaulateTotal());
+            Console.WriteLine(car2.CalaulateTotal());
+            Console.WriteLine(car3.CalaulateTotal());
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //car.TaxPercentage=10;
+            //car1.TaxPercentage=10;
+            //car2.TaxPercentage=10;
+            //car3.TaxPercentage=10;
+            Car.TaxPercentage = 10;
+
+            Console.WriteLine(car.CalaulateTotal());
+            Console.WriteLine(car1.CalaulateTotal());
+            Console.WriteLine(car2.CalaulateTotal());
+            Console.WriteLine(car3.CalaulateTotal());
+        }
+    }
+}
